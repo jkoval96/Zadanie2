2f099dfb0ab38b4eb3a5eab831777a7bf29d3842
Jan Koval <jan.koval96@gmail.com>
2017-04-02 13:05:30 +0200

    Correcting function

--- src/Zadanie2.c
+++ src/Zadanie2.c
@@ -1,11 +1,6 @@
 #include <stdio.h>
 	
 void print_line_segment(char ax, char ay, char bx, char by) {
-	printf("Zadaj x-ovu a y-ovu suradnicu prveho bodu:\n");
-	scanf("%d %d",&ax,&ay);
-	
-	printf("Zadaj x-ovu a y-ovu suradnicu druheho bodu:\n");
-	scanf("%d %d",&bx,&by);
 	
 	printf("%d %d %d %d",ax,ay,bx,by);
 }
@@ -13,7 +8,13 @@ void print_line_segment(char ax, char ay, char bx, char by) {
 	
 	
 int main(void) {
-		char ax,ay,bx,by;
+	char ax,ay,bx,by;
+	
+	printf("Zadaj x-ovu a y-ovu suradnicu prveho bodu:\n");
+	scanf("%d %d",&ax,&ay);
+	
+	printf("Zadaj x-ovu a y-ovu suradnicu druheho bodu:\n");
+	scanf("%d %d",&bx,&by);
 		
-		print_line_segment(ax,ay,bx,by);
+	print_line_segment(ax,ay,bx,by);
 }
