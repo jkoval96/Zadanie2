14977d3794afb309c6ea7b6e6b12cdb7bc663de0
Jan Koval <jan.koval96@gmail.com>
2017-04-02 13:02:40 +0200

    Setting points

--- src/Zadanie2.c
+++ src/Zadanie2.c
@@ -1,11 +1,19 @@
 #include <stdio.h>
 	
 void print_line_segment(char ax, char ay, char bx, char by) {
+	printf("Zadaj x-ovu a y-ovu suradnicu prveho bodu:\n");
+	scanf("%d %d",&ax,&ay);
 	
+	printf("Zadaj x-ovu a y-ovu suradnicu druheho bodu:\n");
+	scanf("%d %d",&bx,&by);
+	
+	printf("%d %d %d %d",ax,ay,bx,by);
 }
 		
 	
 	
 int main(void) {
+		char ax,ay,bx,by;
 		
+		print_line_segment(ax,ay,bx,by);
 }
