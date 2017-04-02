cdf1c3408b8cf6495cf7aa9773fc00778d962400
Jan Koval <jan.koval96@gmail.com>
2017-04-02 13:41:31 +0200

    pretype char to int

--- src/Zadanie2.c
+++ src/Zadanie2.c
@@ -1,14 +1,14 @@
 #include <stdio.h>
 	
-void print_line_segment(char ax, char ay, char bx, char by) {
-	
-	printf("%d %d %d %d", ax, ay, bx, by);
+void print_line_segment(int ax, int ay, int bx, int by) {
+	//using Bresenham's line algorithm
+	printf("%d %d %d %d\n", ax, ay, bx, by);
 }
 		
 	
 	
 int main(void) {
-	char ax, ay, bx, by;
+	int ax, ay, bx, by;
 	
 	printf("Zadaj x-ovu a y-ovu suradnicu prveho bodu:\n");
 	scanf("%d %d", &ax, &ay);
@@ -17,4 +17,5 @@ int main(void) {
 	scanf("%d %d", &bx, &by);
 		
 	print_line_segment(ax, ay, bx, by);
+
 }
