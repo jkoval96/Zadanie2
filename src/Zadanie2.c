4abd8288204b85e7a0bdecc00198932f024a8d88
Jan Koval <jan.koval96@gmail.com>
2017-04-02 13:08:04 +0200

    spaces

--- src/Zadanie2.c
+++ src/Zadanie2.c
@@ -2,19 +2,19 @@
 	
 void print_line_segment(char ax, char ay, char bx, char by) {
 	
-	printf("%d %d %d %d",ax,ay,bx,by);
+	printf("%d %d %d %d", ax, ay, bx, by);
 }
 		
 	
 	
 int main(void) {
-	char ax,ay,bx,by;
+	char ax, ay, bx, by;
 	
 	printf("Zadaj x-ovu a y-ovu suradnicu prveho bodu:\n");
-	scanf("%d %d",&ax,&ay);
+	scanf("%d %d", &ax, &ay);
 	
 	printf("Zadaj x-ovu a y-ovu suradnicu druheho bodu:\n");
-	scanf("%d %d",&bx,&by);
+	scanf("%d %d", &bx, &by);
 		
-	print_line_segment(ax,ay,bx,by);
+	print_line_segment(ax, ay, bx, by);
 }
