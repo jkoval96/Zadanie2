584d99690829d75043327a8b34ee63edbb8198b0
Jan Koval <jan.koval96@gmail.com>
2017-04-03 16:37:10 +0200

    involving field

--- src/Zadanie2.c
+++ src/Zadanie2.c
@@ -2,34 +2,39 @@
 	
 void print_line_segment(int ax, int ay, int bx, int by) {
 	//using Bresenham's line algorithm
-	int dx, dy, D, y, x, i, j;
+	int dx, dy, D, err, y, x, i, j;
 	char field[bx][by];
 	
-	printf("%d %d %d %d\n", ax, ay, bx,by);
+	printf("%d %d %d %d\n", ax, ay, bx, by);
+	
 	dx = bx - ax;
 	dy = by - ay;
 	D = 2*dy - dx;
 	y = ay;
 	
 	for(i = 0; i < ay; i++) {
-		field[0][i] = "\n";
+		field[0][i] = '\n';
 	}
 	for(j = 0; j < ax; j++) {
-		field[j][0] = " ";
+		field[i][0] = 32;
 	}
 	for(x = ax; x <= bx; x++) {
-		printf("x");
+		field[x][y] = 32;
 		if(D > 0) {
 			y = y + 1;
-			printf("\n");
+			field[x][y] = '\n';
 			for(i = 0; i <= x; i++) {
-				printf(" ");
+				field[x][y] = 'x';
 			}
 			D = D - 2*dx;
 		}
 		D = D + 2*dy;
 	}
-	
+	for(i = 0; i < bx; i++) {
+		for(j = 0; j < by; j++) {
+			printf("%c",field[i][j]);
+		}
+	}
 }
 		
 	
