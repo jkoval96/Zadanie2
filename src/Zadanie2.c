75e16d3d9dde787fc7a265cb8a18e55a860cb490
Jan Koval <jan.koval96@gmail.com>
2017-04-03 16:24:03 +0200

    bresenham algorithm

--- src/Zadanie2.c
+++ src/Zadanie2.c
@@ -2,7 +2,34 @@
 	
 void print_line_segment(int ax, int ay, int bx, int by) {
 	//using Bresenham's line algorithm
-	printf("%d %d %d %d\n", ax, ay, bx, by);
+	int dx, dy, D, y, x, i, j;
+	char field[bx][by];
+	
+	printf("%d %d %d %d\n", ax, ay, bx,by);
+	dx = bx - ax;
+	dy = by - ay;
+	D = 2*dy - dx;
+	y = ay;
+	
+	for(i = 0; i < ay; i++) {
+		field[0][i] = "\n";
+	}
+	for(j = 0; j < ax; j++) {
+		field[j][0] = " ";
+	}
+	for(x = ax; x <= bx; x++) {
+		printf("x");
+		if(D > 0) {
+			y = y + 1;
+			printf("\n");
+			for(i = 0; i <= x; i++) {
+				printf(" ");
+			}
+			D = D - 2*dx;
+		}
+		D = D + 2*dy;
+	}
+	
 }
 		
 	
