06d4089740d9f87c68a9ad3e14319bac40b22725
Jan Koval <jan.koval96@gmail.com>
2017-04-09 11:54:02 +0200

    changed field and borders

--- src/Zadanie2.c
+++ src/Zadanie2.c
@@ -3,10 +3,10 @@
 void print_line_segment(int ax, int ay, int bx, int by) {
 	//using Bresenham's line algorithm
 	int dx, dy, D, y, x, i, j;
-	char field[bx][by];
+	char field[by+1][bx+1];
 	
-	for(i = 0; i < bx; i++) {
-		for(j = 0; j < by; j++) {
+	for(i = 0; i <= by; i++) {
+		for(j = 0; j <= bx; j++) {
 			field[i][j] = ' ';
 		}
 	}
@@ -21,16 +21,15 @@ void print_line_segment(int ax, int ay, int bx, int by) {
 		y = ay;
 	
 		for(x = ax; x <= bx; x++) {
-			field[x][y] = 'x';
+			field[y][x] = 'x';
 			if(D > 0) {
 				y = y + 1;
 				D = D - 2*dx;
-			}
+			} else {
 			D = D + 2*dy;
+			}
 		}
-	}
-	
-	else {
+	} else {
 		D = 2*dx - dy;
 		x = ax;
 	
@@ -39,13 +38,14 @@ void print_line_segment(int ax, int ay, int bx, int by) {
 			if(D > 0) {
 				x = x + 1;
 				D = D - 2*dy;
-			}
+			} else {
 			D = D + 2*dx;
+			}
 		}
 	}
 	
-	for(i = 0; i < bx; i++) {
-		for(j = 0; j < by; j++) {
+	for(i = 0; i <= by; i++) {
+		for(j = 0; j <= bx; j++) {
 			printf("%c",field[i][j]);
 		}
 		printf("\n");
